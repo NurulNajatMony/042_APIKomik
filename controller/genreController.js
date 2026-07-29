e1847577b845a9767ca2e88f9b4c2fd5b7b6f7f2
NurulNajatMony <nurulmony505@gmail.com>
2026-07-29 15:11:27 +0700

    menambahkan fitur pencarian genre berdasarkan id

--- controller/genreController.js
+++ controller/genreController.js
@@ -11,3 +11,21 @@ async function getAllGenre(req, res) {
     }
 }
 
+async function getGenreById(req, res) {
+    try {
+        const genre = await db.Genre.findByPk(req.params.id);
+
+        if (!genre) {
+            return res.status(404).json({
+                message: 'Genre tidak ditemukan'
+            });
+        }
+
+        res.status(200).json(genre);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+}
+
