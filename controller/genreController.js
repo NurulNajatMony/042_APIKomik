05eb87463576f6eedb3a30b2d09dfb77b5a1202f
NurulNajatMony <nurulmony505@gmail.com>
2026-07-29 15:13:01 +0700

    mengimplementasikan pembaruan data genre

--- controller/genreController.js
+++ controller/genreController.js
@@ -45,3 +45,25 @@ async function createGenre(req, res) {
     }
 }
 
+async function updateGenre(req, res) {
+    try {
+        const genre = await db.Genre.findByPk(req.params.id);
+
+        if (!genre) {
+            return res.status(404).json({
+                message: 'Genre tidak ditemukan'
+            });
+        }
+
+        genre.nama = req.body.nama;
+
+        await genre.save();
+
+        res.status(200).json(genre);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+}
+
